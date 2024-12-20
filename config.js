81a1e4f332421ccf9fcd710dbd6a468455727071
OfficialHectorManuel <143950168+OfficialHectorManuel@users.noreply.github.com>
2024-12-20 20:14:48 +0000

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "EC1l1RKZ#iIk20jIp2r7-3tmLs0h2qfqTCnPfenAgsis_CVXc-3w",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/n5vvij.jpg",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://i.imgur.com/Dn7wP0C.jpeg",
 SUDO_NB: process.env.SUDO_NB || "263780934873",
 MONGODB: process.env.MONGODB || "mongodb+srv://kulathungaasitha319:yjHB0DvFfStNfwPS@cluster0.3oijd.mongodb.net/",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
